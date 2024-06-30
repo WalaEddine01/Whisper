d946f252f769b81428c54a062b04ad6ed86d3906
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-06-30 23:33:36 +0300

    Client: Fixed some bugs

--- client/src/components/Form/SignupForm.tsx
+++ client/src/components/Form/SignupForm.tsx
@@ -1,4 +1,4 @@
-import { Error, FormElement, Input, InputDiv, Submit } from './Form';
+import { Error, FormElement, Image, Input, InputDiv, Submit } from './Form';
 
 import { Skeleton } from '@mui/material';
 import axios from 'axios';
@@ -16,18 +16,36 @@ const SignupForm = ({ isLoading, setIsLoading }) => {
     formState: { errors },
   } = useForm();
   const navigate = useNavigate();
+  const [imageSrc, setImageSrc] = useState('');
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setIsLoading(true);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageSrc(reader.result);
+        setIsLoading(false);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   async function signUp(data) {
     console.log(data);
     setIsLoading(true);
     try {
       // Make POST request using Axios
-      const response = await axiosInstance.post('/signup', data, {
-        headers: {
-          'Content-Type': 'application/json',
-          // Add any other headers as needed
+      const response = await axiosInstance.post(
+        '/signup',
+        { ...data, imgURL: imageSrc },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers as needed
+          },
         },
-      });
+      );
 
       // Return the response data
       return response.data;
@@ -49,20 +67,23 @@ const SignupForm = ({ isLoading, setIsLoading }) => {
 
   return (
     <FormElement onSubmit={handleSubmit(onSubmit)}>
-      {/* <InputDiv>
-        <Input
-          type="text"
-          name="name"
-          placeholder="Name"
-          {...register('name', {
-            required: {
-              value: true,
-              message: 'Name is required ',
-            },
-          })}
-        />
+      <InputDiv>
+        {!isLoading && (
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'Name is required ',
+              },
+            })}
+          />
+        )}
+        {isLoading && <Skeleton variant="rectangular" height={64} />}
         {errors.name && <Error>* {errors.name.message}</Error>}
-      </InputDiv> */}
+      </InputDiv>
       <InputDiv>
         {!isLoading && (
           <Input
@@ -113,6 +134,26 @@ const SignupForm = ({ isLoading, setIsLoading }) => {
         )}
         {errors.password && <Error>* {errors.password.message}</Error>}
         {isLoading && <Skeleton variant="rectangular" height={64} />}
+      </InputDiv>
+      <InputDiv>
+        <input
+          type="file"
+          style={{ display: 'none' }}
+          id="fileInput"
+          onChange={handleFileChange}
+        />
+        <label htmlFor="fileInput">
+          <Image>
+            {isLoading ? (
+              <Skeleton variant="circular" width={64} height={64} />
+            ) : (
+              <>
+                {imageSrc && <img src={imageSrc} alt="Profile Image" />}
+                {!imageSrc && <p>+</p>}
+              </>
+            )}
+          </Image>
+        </label>
       </InputDiv>
       <InputDiv>
         {!isLoading && <Submit type="submit" value="SignUp" name="SignUp" />}
